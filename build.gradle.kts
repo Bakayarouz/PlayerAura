ed02ccaa26906db798fbcffe969eec732edd444f
Bakayarouz <56614842+Bakayarouz@users.noreply.github.com>
2026-09-20 23:29:51 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,13 @@ java {
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 dependencies {
     // Paper API dependency (works across all 1.21.x sub-versions)
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
 tasks {
